f0a721fca53230908cbadb0b305ab3f758d74e50
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-12 21:14:51 +0200

    init

---
+++ server/middelware/secureAdmin.js
@@ -0,0 +1,17 @@
+const secureAdmin = () => {
+  return (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next();
+      } else {
+        req.flash("error", "ADMIN USER REQUIRED");
+
+        return res.redirect("/api/unauthorized");
+      }
+    }
+
+    res.redirect("/api/login");
+  };
+};
+
+module.exports = secureAdmin;
