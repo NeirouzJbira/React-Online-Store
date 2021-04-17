bfa2b0b024b28f55535970331b55d0bef304e682
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-17 22:10:10 +0200

    init

---
+++ client/src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import "./mainStyles/bootstrapStyles.scss";
+import "./mainStyles/index.scss";
+import theme from "./styledComponents/theme";
+import App from "./App";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import store from "./redux/store";
+import SignupValidation from "./pages/Signup/SignupValidation";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword/ResetPassword";
+import ChangePassword from "./pages/ChangePassword/ChangePassword";
+
+const destination = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/login/forgotpassword"
+            component={ForgotPassword}
+          />
+          <Route exact path="/users/resetpassword" component={ResetPassword} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/users/validate" component={SignupValidation} />
+          <Route component={App} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  </Provider>,
+  destination
+);
