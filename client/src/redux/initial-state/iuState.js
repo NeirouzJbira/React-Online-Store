097737f593292de6f45837c947dabc7954fbb913
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-18 02:05:34 +0200

    init

---
+++ client/src/redux/initial-state/iuState.js
@@ -0,0 +1,9 @@
+const iuState = {
+
+	departmentsOpen: false,
+	goCart: false,	// PARA USER CON <Redirect to="/cart" /> EN LOS COMPONENTES QUE LO REQUIRAN
+	goLogin: false // PARA USER CON <Redirect to="/login" /> EN LOS COMPONENTES QUE LO REQUIRAN
+	
+};
+
+export default iuState;
