2042847f5d901992bbd85096500a59bf752cbc38
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-18 02:06:28 +0200

    init

---
+++ client/src/redux/initial-state/initialState.js
@@ -0,0 +1,20 @@
+import loggedUser from './loggedUser';
+import itemsData from './itemsData';
+import usersData from './usersData';
+import banners from './banners';
+import videos from './videos';
+import errorMessages from './errorMessages';
+import iuState from './iuState';
+import cart from './cart';
+
+
+const initialState = {
+	loggedUser,
+	usersData,
+	itemsData,
+	errorMessages,
+	iuState,
+	cart
+};
+
+export default initialState;
