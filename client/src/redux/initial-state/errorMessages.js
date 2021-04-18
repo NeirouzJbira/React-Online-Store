6734463d55ac63adbf231d313c19e2f1995a4e07
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-18 02:05:30 +0200

    init

---
+++ client/src/redux/initial-state/errorMessages.js
@@ -0,0 +1,20 @@
+const errorMessages = {
+
+	'001': 'Email already exists in our database!',
+	'002': 'There was a problem verifying your account, we have sent you a new email. Please check your inbox and try again.',
+	'003': 'Your account appears to be already validated in our database. Please click the button below to Log In.',
+	'004': '. Please go back to your email verification message and try again',
+	'005': 'Your account has not been verified. Please check your email inbox and follow the instructions to verify your account.',
+	'006': 'Your account has been temporarly locked due too many failed login attemps. Please wait a few minutes and try again.',
+	'007': 'Email not found in the database!',
+	'008': 'Incorrect Password!',
+	'009': 'Internal Server Error : ',
+	'010': '. Please check your Internet Connection and try again.',
+	'011': 'Error status 400 - Bad Request: ',
+	'012': 'The passwords you entered do not match. Please re-enter your password',
+	'013': 'Connection Error: '
+
+};
+
+
+export default errorMessages;
