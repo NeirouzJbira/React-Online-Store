d381733267ddc09e0adb7575c2cc9efafb49f22e
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-19 04:38:00 +0200

    init

---
+++ client/src/components/Departments/Departments.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DepartmentsLarge from '../DepartmentsLarge/DepartmentsLarge';
+import DepartmentsSmall from '../DepartmentsSmall/DepartmentsSmall';
+
+function Departments() {
+	return (
+		<>
+			<DepartmentsLarge />
+			<DepartmentsSmall />
+		</>
+	);
+}
+
+export default Departments;
