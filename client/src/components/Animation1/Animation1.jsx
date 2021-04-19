5c7c1056d107ce8318e00b4bb6ce3277fc838c4a
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-19 04:23:06 +0200

    init

---
+++ client/src/components/Animation1/Animation1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import video from '../../assets/images/OFFICE.jpg';
+import './Animation1.scss';
+
+function Animation1() {
+	return (
+		<div id="animation1-gif">
+			<img src={video} alt="" />	
+		</div>
+	);
+}
+
+export default Animation1;
