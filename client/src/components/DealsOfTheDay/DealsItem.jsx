28ebc90accabf32f73e626afbf4d9f6ca211a29a
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-21 06:33:31 +0200

    init

--- client/src/components/DealsOfTheDay/DealsItem.jsx
+++ client/src/components/DealsOfTheDay/DealsItem.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './DealsItem.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./DealsItem.scss";
 
 function DealsItem({ itemObject, imageObject }) {
+  const itemPath = `/itemsheet/${itemObject._id}`;
 
-	const itemPath = `/itemsheet/${itemObject._id}`;
-
-	return (
-		<div className="deals-item">
-			<p className="deals-item-caption">{itemObject.shortName}</p>					
-			<div className="deals-items-image-container">
-				<div className="deals-items-image-center">
-					<img src={imageObject.imageURL} alt="" className="deals-items-image" />
-					<Link to={itemPath}>
-						<button type="button" className="deals-items-button">Quick Look</button>
-					</Link>
-				</div>
-				
-			</div>
-		</div>
-	);
+  return (
+    <div className="deals-item">
+      <p className="deals-item-caption">{itemObject.shortName}</p>
+      <div className="deals-items-image-container">
+        <div className="deals-items-image-center">
+          <img
+            src={imageObject.imageURL}
+            alt=""
+            className="deals-items-image"
+          />
+          <Link to={itemPath}>
+            <button type="button" className="deals-items-button">
+              Quick Look
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DealsItem;
