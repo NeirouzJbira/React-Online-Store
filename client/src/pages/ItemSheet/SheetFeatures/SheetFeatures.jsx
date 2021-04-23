4f75453263e07f2db096b325fc6522a6cb935732
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-23 23:05:22 +0200

    init

--- client/src/pages/ItemSheet/SheetFeatures/SheetFeatures.jsx
+++ client/src/pages/ItemSheet/SheetFeatures/SheetFeatures.jsx
@@ -1,78 +1,65 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-
-	grid-area: features;
-	padding: 20px;
-	background-color: white;
-
+  grid-area: features;
+  padding: 20px;
+  background-color: white;
 `;
 
 const StyledTitle = styled.h6`
-
-	color: #354069;
-	padding: 0;
-	margin: 0;
-	font-family: "Lilita One", Verdana, Geneva, Tahoma, sans-serif;
-	padding-left: 20px;
-	font-size: 1.5rem;
+  color: #354069;
+  padding: 0;
+  margin: 0;
+  font-family: "Lilita One", Verdana, Geneva, Tahoma, sans-serif;
+  padding-left: 20px;
+  font-size: 1.5rem;
 `;
 
 const StyledList = styled.ul`
-
-	list-style: none;
-	
-
+  list-style: none;
 `;
 
 const StyledListLine = styled.li`
-
-	padding-left: 20px;
-
+  padding-left: 20px;
 `;
 
 const StyledLineTitle = styled.p`
-
-	padding: 0;
-	margin: 0;
-	font-size: 1.2rem;
-	font-weight: bold;
-	color: rgba(0,0,0,.7);
+  padding: 0;
+  margin: 0;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: rgba(0, 0, 0, 0.7);
 `;
 
-
 const StyledLineDescription = styled.p`
-
-	padding: 0;
-	margin: 0;
-	font-size: 1.2rem;
-
+  padding: 0;
+  margin: 0;
+  font-size: 1.2rem;
 `;
 
 function SheetFeatures({ keyFeatures }) {
+  const listLine = (index, title, description) => {
+    return (
+      <StyledListLine key={index}>
+        {title ? <StyledLineTitle>- {title}</StyledLineTitle> : null}
+        {description ? (
+          <StyledLineDescription>{description}</StyledLineDescription>
+        ) : null}
+      </StyledListLine>
+    );
+  };
 
-	
-	const listLine = (index, title, description) => {
-
-		return (
-			<StyledListLine key={index}>
-				{ title ? <StyledLineTitle>- {title}</StyledLineTitle> : null }
-				{ description ? <StyledLineDescription>{description}</StyledLineDescription> : null }
-			</StyledListLine>
-		);
-
-	};
-
-
-	return (
-		<Container>
-			<StyledTitle>Key Features:</StyledTitle>
-			<StyledList>
-				{ keyFeatures.map((item, index) => listLine(index, item.title, item.description))}
-			</StyledList>
-		</Container>
-	);
+  return (
+    <Container>
+      <StyledTitle>Key Features:</StyledTitle>
+      <StyledList>
+        {keyFeatures.map((item, index) =>
+          listLine(index, item.title, item.description)
+        )}
+      </StyledList>
+    </Container>
+  );
 }
 
 export default SheetFeatures;
