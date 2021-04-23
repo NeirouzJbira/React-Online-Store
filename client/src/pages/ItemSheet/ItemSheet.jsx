af2972fcd8b261524b6dcdb223641c55d2c6480a
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-23 23:05:02 +0200

    init

--- client/src/pages/ItemSheet/ItemSheet.jsx
+++ client/src/pages/ItemSheet/ItemSheet.jsx
@@ -1,130 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams, useHistory } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Sheet from './Sheet/Sheet';
-import AddedToCart from '../AddedToCart/AddedToCart';
-import { fetchPostCartItem } from '../../modules/fetchFunctions/cart';
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+import Sheet from "./Sheet/Sheet";
+import AddedToCart from "../AddedToCart/AddedToCart";
+import { fetchPostCartItem } from "../../modules/fetchFunctions/cart";
 
-import { actionsAsyncFetchSelectedItem, actionsAsyncFetchGetCartItems } from '../../redux/actions/asyncActions';
 import {
-	getIsSelectedItemFetching,
-	getIsSelectedItemError,
-	getSelectedItemErrorMessage,
-	getSelectedItem,
-	getSelectedItemImages,
-	getIsLoggedUser,
-	getUserProfile,
-	getCartTotals
-} from '../../redux/selectors';
-import Loading from '../../components/Loading1/Loading';
-import FetchError from '../../components/FetchError/FetchError';
+  actionsAsyncFetchSelectedItem,
+  actionsAsyncFetchGetCartItems,
+} from "../../redux/actions/asyncActions";
+import {
+  getIsSelectedItemFetching,
+  getIsSelectedItemError,
+  getSelectedItemErrorMessage,
+  getSelectedItem,
+  getSelectedItemImages,
+  getIsLoggedUser,
+  getUserProfile,
+  getCartTotals,
+} from "../../redux/selectors";
+import Loading from "../../components/Loading1/Loading";
+import FetchError from "../../components/FetchError/FetchError";
 
+function ItemSheet({
+  dispatch,
+  isFetching,
+  isFetchingError,
+  getErrorMessage,
+  getItem,
+  getImages,
+  isUserLogged,
+  userProfile,
+  cartTotals,
+}) {
+  const { itemId } = useParams();
 
-function ItemSheet({ dispatch, isFetching, isFetchingError, getErrorMessage, getItem, getImages, isUserLogged, userProfile, cartTotals }) {
+  const [showAddedToCart, setShowAddedToCart] = useState(false);
+  const [quant, setQuant] = useState(0);
 
-	const { itemId } = useParams();
+  const history = useHistory();
 
-	const [showAddedToCart, setShowAddedToCart] = useState(false);
-	const [quant, setQuant] = useState(0);
-	
+  const handleAddToCart = (quantity) => {
+    const qty = Number.parseInt(quantity, 10);
 
-	const history = useHistory();
+    if (qty > 0) {
+      if (!isUserLogged) {
+        setShowAddedToCart(false);
 
-	const handleAddToCart = quantity => {
+        history.push("/login");
+      } else {
+        setQuant(qty);
 
-		const qty = Number.parseInt(quantity, 10);
-		
-				
-		if (qty > 0) {
-			
-			if (!isUserLogged) {
+        fetchPostCartItem(userProfile._id, itemId, qty).then(() => {
+          dispatch(actionsAsyncFetchGetCartItems(userProfile._id)).then(() => {
+            setShowAddedToCart(true);
+          });
+        });
+      }
+    }
+  };
 
-				setShowAddedToCart(false);
+  useEffect(() => {
+    dispatch(actionsAsyncFetchSelectedItem(itemId)).then(() => {
+      dispatch(actionsAsyncFetchGetCartItems(userProfile._id)).then(() => {});
+    });
+  }, []);
 
-				history.push('/login');
+  let content = null;
 
-			} else {
-				
-				setQuant(qty);
+  if (isFetching) {
+    content = <Loading type="arrow" />;
+  } else if (isFetchingError) {
+    content = <FetchError type="boy" message={getErrorMessage} />;
+  } else if (showAddedToCart) {
+    content = (
+      <div style={{ width: "100%", position: "relative", opacity: 1 }}>
+        <AddedToCart
+          itemObject={getItem}
+          itemImages={getImages}
+          quantity={quant}
+          cartTotals={cartTotals}
+        />
+        <Sheet
+          itemObject={getItem}
+          itemImages={getImages}
+          handleAddToCart={handleAddToCart}
+          lowOpacity
+        />
+      </div>
+    );
+  } else {
+    content = (
+      <Sheet
+        itemObject={getItem}
+        itemImages={getImages}
+        handleAddToCart={handleAddToCart}
+      />
+    );
+  }
 
-				fetchPostCartItem(userProfile._id, itemId, qty).then(() => {
-						
-					dispatch(actionsAsyncFetchGetCartItems(userProfile._id)).then(() => {							
-						
-											
-						setShowAddedToCart(true);	
-
-					});
-
-				});
-
-			}
-		}	
-	};
-
-
-	useEffect(() => {
-	
-		dispatch(actionsAsyncFetchSelectedItem(itemId)).then(() => {
-			
-			dispatch(actionsAsyncFetchGetCartItems(userProfile._id)).then(() => {
-
-
-			});
-		
-		});
-			
-	}, []);
-
-
-	let content = null;
-
-	if (isFetching) {
-
-		content = (<Loading type="arrow" />);
-
-	} else if (isFetchingError) {
-		
-		content = (<FetchError type="boy" message={getErrorMessage} />);
-
-	} else if (showAddedToCart) {
-
-		content = (
-			<div style={{ width: '100%', position: 'relative', opacity: 1 }}>
-				<AddedToCart itemObject={getItem} itemImages={getImages} quantity={quant} cartTotals={cartTotals} />
-				<Sheet itemObject={getItem} itemImages={getImages} handleAddToCart={handleAddToCart} lowOpacity />
-			</div>
-		);
-		
-	} else {
-
-		content = (<Sheet itemObject={getItem} itemImages={getImages} handleAddToCart={handleAddToCart} />);
-
-
-	}
-
-	
-	return (
-		<>
-			{content}
-		</>
-	);
+  return <>{content}</>;
 }
 
-
-const mapStateToProps = state => {
-
-	return {
-		isFetching: getIsSelectedItemFetching(state),
-		isFetchingError: getIsSelectedItemError(state),
-		getErrorMessage: getSelectedItemErrorMessage(state),
-		getItem: getSelectedItem(state),
-		getImages: getSelectedItemImages(state),
-		isUserLogged: getIsLoggedUser(state),
-		userProfile: getUserProfile(state),
-		cartTotals: getCartTotals(state)
-	};
-
+const mapStateToProps = (state) => {
+  return {
+    isFetching: getIsSelectedItemFetching(state),
+    isFetchingError: getIsSelectedItemError(state),
+    getErrorMessage: getSelectedItemErrorMessage(state),
+    getItem: getSelectedItem(state),
+    getImages: getSelectedItemImages(state),
+    isUserLogged: getIsLoggedUser(state),
+    userProfile: getUserProfile(state),
+    cartTotals: getCartTotals(state),
+  };
 };
 
 export default connect(mapStateToProps, null)(ItemSheet);
