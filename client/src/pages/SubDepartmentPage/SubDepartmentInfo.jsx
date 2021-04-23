bfb1331a3feaf02ba22b33c15d453147ad25f8de
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-23 23:07:01 +0200

    init

--- client/src/pages/SubDepartmentPage/SubDepartmentInfo.jsx
+++ client/src/pages/SubDepartmentPage/SubDepartmentInfo.jsx
@@ -1,63 +1,45 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledContainer = styled.div`
+  width: 100%;
 
-	width: 100%;
-
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 const StyledImage = styled.img`
-
-	max-width: 50px;
-
+  max-width: 50px;
 `;
 
-const StyledDescriptionContainer = styled.div`
-
-
-
-`;
+const StyledDescriptionContainer = styled.div``;
 
 const StyledNames = styled.h6`
-
-	padding: 0;
-	margin: 0;
-
+  padding: 0;
+  margin: 0;
 `;
 
-const StyledDescription = styled.p`
-
-
-
-`;
+const StyledDescription = styled.p``;
 
 function SubDepartmentInfo({ subdepObject, subdepImage }) {
+  const { name, description } = subdepObject;
 
-	const { name, description } = subdepObject;
+  const { name: categoryName } = subdepObject.category;
 
-	const { name: categoryName } = subdepObject.category;
+  const { imageURL } = subdepImage;
 
-	const { imageURL } = subdepImage;
-
-
-	return (
-		<StyledContainer>
-			<StyledImage src={imageURL} alt="" />
-			<StyledDescriptionContainer>
-				<StyledNames>
-					{name} | {categoryName}
-				</StyledNames>	
-				<StyledDescription>
-					{description}
-				</StyledDescription>
-			</StyledDescriptionContainer>			
-		</StyledContainer>
-	);
+  return (
+    <StyledContainer>
+      <StyledImage src={imageURL} alt="" />
+      <StyledDescriptionContainer>
+        <StyledNames>
+          {name} | {categoryName}
+        </StyledNames>
+        <StyledDescription>{description}</StyledDescription>
+      </StyledDescriptionContainer>
+    </StyledContainer>
+  );
 }
 
 export default SubDepartmentInfo;
