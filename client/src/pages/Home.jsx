5b561cb25933b2ff9345f0b5578208110021f64b
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-23 23:04:00 +0200

    init

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import Deals from '../components/DealsOfTheDay/Deals';
-import Animation1 from '../components/Animation1/Animation1';
-import Season from '../components/SeasonItems/Season';
-import './Home.scss';
-
+import React, { Component } from "react";
+import Deals from "../components/DealsOfTheDay/Deals";
+import Animation1 from "../components/Animation1/Animation1";
+import Season from "../components/SeasonItems/Season";
+import "./Home.scss";
 
 export default class Home extends Component {
-	render() {
-		return (
-			<>
-				<div id="home-grid">
-					<Deals />				
-					<Animation1 />
-					<Season />
-				</div>
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <div id="home-grid">
+          <Deals />
+          <Animation1 />
+          <Season />
+        </div>
+      </>
+    );
+  }
 }
